c241f4ff67488dbfc859223b41e450a9455c5c08
Don Reeves <donjreeves@gmail.com>
2021-12-19 21:47:04 -0500

    intial commit

--- move.js
+++ move.js
@@ -4,9 +4,63 @@ function move(element) {
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
+        // element.style.zIndex = zIndex + 'px'
+    }
+
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+    
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+        
+        function moveCharacter(){ 
+            if(direction === 'west' && character.x >= window.screenLeft){
+                x-=1
+            }
+            if(direction === 'north' && character.y >= window.screenTop){
+                y+=1
+            }
+            if(direction === 'east' && character.x <= 1000){
+                x+=1
+            }
+            if(direction === 'south' && character.y <= 800){
+                y-=1
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+        
+        setInterval(moveCharacter, 1)
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+        
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+                direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+                direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            callback(direction)
+            
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null
+            callback(direction)
+        })
     }
 
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
